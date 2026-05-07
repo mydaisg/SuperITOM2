5282be4ab608296040ab99f793dee07eae40360e
mydaisg <my@daisg.com>
2026-05-07 10:40:47 +0800

    change start env

--- start_app.bat
+++ start_app.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d D:\GitHub\SuperITOM2
-echo Starting app...
-Rscript -e "shiny::runApp('.', port=3838, host='0.0.0.0', launch.browser=FALSE)" 2>&1
+echo Starting SuperITOM2...
+echo R Version: 4.6.0
+"D:\Tai_Programs\R-4.6.0\bin\Rscript.exe" -e "shiny::runApp('.', port=3838, host='0.0.0.0', launch.browser=FALSE)" 2>&1
 echo Exit code: %ERRORLEVEL%
 pause
