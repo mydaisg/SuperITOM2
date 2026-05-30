8c2dc4ac1233e1f99fadb02b9b3645bd2fc57926
mydaisg <my@daisg.com>
2026-05-30 10:59:08 +0800

    Error Version（Login Blank）

--- run_app.R
+++ run_app.R
@@ -1,5 +1,5 @@
 setwd("D:/GitHub/SuperITOM2")
 source("global.R")  # 加载全局配置和库
 # shiny::runApp(".", port=80, host="127.0.0.1", launch.browser=FALSE)
-# shiny::runApp(".", port=3838, host="0.0.0.0", launch.browser=FALSE)
-shiny::runApp(".", port=8000, host="0.0.0.0", launch.browser=FALSE)
+shiny::runApp(".", port=3838, host="0.0.0.0", launch.browser=FALSE)
+# shiny::runApp(".", port=8000, host="0.0.0.0", launch.browser=FALSE)
